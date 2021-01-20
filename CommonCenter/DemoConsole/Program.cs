25d98c26afb5dc89c09d076567b87bdba54dd59a
fred tu <ran.tu@shengkunforce.com>
2021-01-20 15:54:20 +0800

    update

--- CommonCenter/DemoConsole/Program.cs
+++ CommonCenter/DemoConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace DemoConsole
 {
@@ -7,37 +8,7 @@ namespace DemoConsole
     {
         static void Main(string[] args)
         {
-            CommonService.Excel.Export export = new CommonService.Excel.Export(CommonService.Excel.Common.ExcelVersion.Excel2007);
-            export.Sheet("ClassA_Sheet", new List<Models.OjbectCreate.ClassA>() {
-                new Models.OjbectCreate.ClassA(){
-                     ID="123",
-                      Name="Fred"
-                },
-                new Models.OjbectCreate.ClassA()
-                {
-                     ID="456",
-                      Name="Amanada"
-                },
-                new Models.OjbectCreate.ClassA()
-                {
-                     ID="789",
-                      Name="Jason"
-                }
-            });
-
-            export.Save(@"D:\a.xlsx");
-            Console.WriteLine("Export Completed");
             Console.ReadKey();
-        }
-
-
-        static long StopWatcher(Action action)
-        {
-            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-            stopwatch.Start();
-            action();
-            stopwatch.Stop();
-            return stopwatch.ElapsedMilliseconds;
         }
     }
 }
