acf48495934a7cefcb99b5ca11a21a87376854cc
fred tu <ran.tu@shengkunforce.com>
2021-01-21 17:33:17 +0800

    add opencvsharp support. add basic image process functions

--- CommonCenter/DemoConsole/Program.cs
+++ CommonCenter/DemoConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using OpenCVService;
 
 namespace DemoConsole
 {
@@ -8,6 +9,17 @@ namespace DemoConsole
     {
         static void Main(string[] args)
         {
+            var input_path = @"C:\Users\shtr0\Pictures\gyy.jpg";
+            var output_path = @"C:\Users\shtr0\Pictures\Export\a.jpg";
+
+            using (Image image = new Image(input_path))
+            {
+                image.ReSize(new OpenCvSharp.Size(image.Width / 2, image.Height / 2));
+                image.EdgePreservingFilter(OpenCvSharp.EdgePreservingMethods.NormconvFilter,5,0.45f);
+                image.Show(Image.ShowType.Changed);
+            }
+            Console.WriteLine("Completed");
+
             Console.ReadKey();
         }
     }
